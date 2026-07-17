9b690aa9753033126a26f63b8bc1a6611b7ed451
Niklas Lindgren <me@sigurs.io>
2026-07-17 13:44:00 +0300

    add vnc to obs and set uid

---
+++ github.com/obsproject/obs-studio/start-obs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Runs under xvfb-run, so DISPLAY + XAUTHORITY are already set for us.
+# Serve that framebuffer over VNC, then hand off to OBS.
+# ponytail: -nopw, fine for a LAN/dev container. Set VNC_PASSWORD to require one.
+set -e
+
+if [ -n "$VNC_PASSWORD" ]; then
+    AUTH="-passwd $VNC_PASSWORD"
+else
+    AUTH="-nopw"
+fi
+
+x11vnc -display "$DISPLAY" -forever -shared -rfbport 5900 -quiet $AUTH &
+
+exec obs
